c447b79202d21a93b9b8b6b5f3aecee0bb3ec3f9
Matteo Zanoncello <matteozanoncello.dev@gmail.com>
2025-12-01 22:26:18 +0100

    2025 day 1

---
+++ src/2025/2025.test.ts
@@ -0,0 +1,104 @@
+import { testDay } from '../utils/tests'
+
+describe('2025', () => {
+  it(`Day 1`, async () => {
+    await testDay(2025, 1, 'sample', [], 3, 6)
+    await testDay(2025, 1, 'input', [], 1172, 6932)
+  })
+  // it(`Day 2`, async () => {
+  //   await testDay(2025, 2, 'sample', [], null, null)
+  //   await testDay(2025, 2, 'input', [], null, null)
+  // })
+  // it(`Day 3`, async () => {
+  //   await testDay(2025, 3, 'sample', [], null, null)
+  //   await testDay(2025, 3, 'input', [], null, null)
+  // })
+  // it(`Day 4`, async () => {
+  //   await testDay(2025, 4, 'sample', [], null, null)
+  //   await testDay(2025, 4, 'input', [], null, null)
+  // })
+  // it(`Day 5`, async () => {
+  //   await testDay(2025, 5, 'sample', [], null, null)
+  //   await testDay(2025, 5, 'input', [], null, null)
+  // })
+  // xit(`Day 6`, async () => {
+  //   await testDay(2025, 6, 'sample', [], null, null)
+  //   await testDay(2025, 6, 'input', [], null, null)
+  // })
+  // xit(`Day 7`, async () => {
+  //   await testDay(2025, 7, 'sample', [], null, null)
+  //   await testDay(2025, 7, 'input', [], null, null)
+  // })
+  // it(`Day 8`, async () => {
+  //   await testDay(2025, 8, 'sample', [], null, null)
+  //   await testDay(2025, 8, 'input', [], null, null)
+  // })
+  // it(`Day 9`, async () => {
+  //   await testDay(2025, 9, 'sample', [], null, null)
+  //   await testDay(2025, 9, 'input', [], null, null)
+  // })
+  // it(`Day 10`, async () => {
+  //   await testDay(2025, 10, 'sample', [], null, null)
+  //   await testDay(2025, 10, 'input', [], null, null)
+  // })
+  // it(`Day 11`, async () => {
+  //   await testDay(2025, 11, 'sample', [], null, null)
+  //   await testDay(2025, 11, 'input', [], null, null)
+  // })
+  // it(`Day 12`, async () => {
+  //   await testDay(2025, 12, 'sample', [], null, null)
+  //   await testDay(2025, 12, 'input', [], null, null)
+  // })
+  // it(`Day 13`, async () => {
+  //   await testDay(2025, 13, 'sample', [], null, null)
+  //   await testDay(2025, 13, 'input', [], null, null)
+  // })
+  // fit(`Day 14`, async () => {
+  //   await testDay(2025, 14, 'sample', [], null, null)
+  //   await testDay(2025, 14, 'input', [], null, null)
+  // })
+  // it(`Day 15`, async () => {
+  //   await testDay(2025, 15, 'sample', [], null, null)
+  //   await testDay(2025, 15, 'input', [], null, null)
+  // })
+  // it(`Day 16`, async () => {
+  //   await testDay(2025, 16, 'sample', [], null, null)
+  //   await testDay(2025, 16, 'input', [], null, null)
+  // })
+  // it(`Day 17`, async () => {
+  //   await testDay(2025, 17, 'sample', [], null, null)
+  //   await testDay(2025, 17, 'input', [], null, null)
+  // })
+  // xit(`Day 18`, async () => {
+  //   await testDay(2025, 18, 'sample', [], null, null)
+  //   await testDay(2025, 18, 'input', [], null, null)
+  // })
+  // it(`Day 19`, async () => {
+  //   await testDay(2025, 19, 'sample', [], null, null)
+  //   await testDay(2025, 19, 'input', [], null, null)
+  // })
+  // it(`Day 20`, async () => {
+  //   await testDay(2025, 20, 'sample', [], null, null)
+  //   await testDay(2025, 20, 'input', [], null, null)
+  // })
+  // it(`Day 21`, async () => {
+  //   await testDay(2025, 21, 'sample', [], null, null)
+  //   await testDay(2025, 21, 'input', [], null, null)
+  // })
+  // it(`Day 22`, async () => {
+  //   await testDay(2025, 22, 'sample', [], null, null)
+  //   await testDay(2025, 22, 'input', [], null, null)
+  // })
+  // it(`Day 23`, async () => {
+  //   await testDay(2025, 23, 'sample', [], null, null)
+  //   await testDay(2025, 23, 'input', [], null, null)
+  // })
+  // it(`Day 24`, async () => {
+  //   await testDay(2025, 24, 'sample', [], null, null)
+  //   await testDay(2025, 24, 'input', [], null, null)
+  // })
+  // it(`Day 25`, async () => {
+  //   await testDay(2025, 25, 'sample', [], null, null)
+  //   await testDay(2025, 25, 'input', [], null, null)
+  // })
+})
---
+++ src/2025/day1/index.ts
@@ -0,0 +1,70 @@
+import { readFile } from '../../utils/io'
+import { DaySolution } from '../../utils/type'
+
+interface Rotation {
+  dir: number
+  steps: number
+}
+
+function rotate(cursor: number, dir: number, steps: number): number {
+  const shift = dir * steps
+  return (cursor + shift + 100) % 100
+}
+
+function resolvePart1(rotations: Rotation[]): number {
+  let cursor = 50
+  let res = 0
+  rotations.forEach((rotation) => {
+    cursor = rotate(cursor, rotation.dir, rotation.steps)
+    if (cursor === 0) res++
+  })
+  return res
+}
+
+function resolvePart2(rotations: Rotation[]): number {
+  let cursor = 50
+  let res = 0
+  rotations.forEach(({ dir, steps }) => {
+    // compute steps to zero
+    const stepsToZero = dir === 1 ? 100 - cursor : cursor ? cursor : 100
+    // if we can't reach zero in this rotation. move cursor and return
+    if (steps < stepsToZero) {
+      cursor = rotate(cursor, dir, steps)
+      return
+    }
+
+    // otherwise, we will reach zero at least once
+    res++
+    cursor = 0
+    // if we reach zero exactly at the end of rotation, return
+    if (steps === stepsToZero) return
+
+    // otherwise, compute remaining steps after first reach to zero
+    const remainingSteps = steps - stepsToZero
+    // each 100 steps we reach zero again
+    res += Math.floor(remainingSteps / 100)
+
+    // compute last steps after last reach to zero
+    const lastSteps = remainingSteps % 100
+    cursor = rotate(cursor, dir, lastSteps)
+  })
+  return res
+}
+
+export default async function (inputFile: string): Promise<DaySolution> {
+  const input = await readFile(inputFile).then((text) => text.trim())
+  const rotations: Rotation[] = input.split('\n').map((line) => {
+    const dir = line[0] === 'L' ? -1 : 1
+    const steps = parseInt(line.slice(1))
+    return { dir, steps }
+  })
+
+  const t0 = performance.now()
+
+  let part1 = null //  resolvePart1(rotations)
+  let part2 = resolvePart2(rotations)
+
+  const t1 = performance.now()
+
+  return [part1, part2, t1 - t0]
+}
